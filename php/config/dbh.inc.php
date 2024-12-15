08c7c92dfe925c4d8c0790d876911eae041f7eda
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-12-15 10:41:36 +0300

    host

--- php/config/dbh.inc.php
+++ php/config/dbh.inc.php
@@ -1,13 +1,13 @@
 <?php
 // DATABASE CONNECTION
-$dsn = "mysql:host=127.0.0.1;dbname=u812004406_montest";
+$dsn = "mysql:host=localhost;dbname=u812004406_montest";
 $dbusername = "u812004406_root";
 $dbpassword = "Root2002!";
 
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "connected";
+    // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
 } catch (PDOException $e) {
     echo json_encode(['error' => 'Connection failed: ' . $e->getMessage()]);
     error_log('Connection failed: ' . $e->getMessage());
